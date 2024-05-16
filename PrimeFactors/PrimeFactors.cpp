a6053fdac6c2e66973143cec8409f37205944322
TaeHong Kim <artoflunar@gmail.com>
2024-05-16 17:02:55 +0900

    [refactor] 변수 이름 변경

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -7,21 +7,19 @@ class PrimeFactors {
 public:
 	vector<int> of(int value)
 	{
-		int div;
+		int rem = value;
 		vector<int> v;
 		int n = 2;
 
-		div = value;
-
 		do {
-			if ((div % n) == 0) {
-				div /= n;
+			if ((rem % n) == 0) {
+				rem /= n;
 				v.push_back(n);
 				n = 2;
 			}
 			else
 				n++;
-		} while (n <= div);
+		} while (n <= rem);
 			
 		return v;
 	}
