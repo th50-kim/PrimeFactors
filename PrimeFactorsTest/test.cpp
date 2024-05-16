c636011453a05b188c3cacf04b76e1fdb2030885
TaeHong Kim <artoflunar@gmail.com>
2024-05-16 16:57:50 +0900

    [feature] tc 추가

--- PrimeFactorsTest/test.cpp
+++ PrimeFactorsTest/test.cpp
@@ -33,4 +33,24 @@ TEST(PrimeFactors, OfTest4) {
 	EXPECT_EQ(expected, pf.of(6));
 }
 
-	
+TEST(PrimeFactors, OfTest5) {
+
+	PrimeFactors pf;
+	vector<int> expected = { 2,2,2 };
+	EXPECT_EQ(expected, pf.of(8));
+}
+
+TEST(PrimeFactors, OfTest6) {
+
+	PrimeFactors pf;
+	vector<int> expected = { 2,2,3 };
+	EXPECT_EQ(expected, pf.of(12));
+}
+
+TEST(PrimeFactors, OfTest7) {
+
+	PrimeFactors pf;
+	vector<int> expected = { 2,7 };
+	EXPECT_EQ(expected, pf.of(14));
+}
+
